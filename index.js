2b9776683b8634b1e3d9eb194ce19d58c498f30a
Lukas Krneta <lukas.thekrneta@gmail.com>
2025-07-27 09:40:20 +0200

    decided to do an EJS frontend

--- index.js
+++ index.js
@@ -11,13 +11,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 const db = new pg.Client({
     user: "postgres",
     host: "localhost",
-    database: "world",
+    database: "Capstone",
     password: "123",
     port: 5432,
 });
 
 db.connect();
 
-app.use('/', async (req, res) => {
-    res.sendFile("index.html");
+const API_address = "https://openlibrary.org/search.json?q="
+
+app.post('/new', async (req, res) => {
+    const input = req.body.query;
+    const result = await fetch(API_address + input);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
