b4422ce148feaf550dae6e452273b8673fd4c74e
Lukas Krneta <lukas.thekrneta@gmail.com>
2025-07-27 17:35:48 +0200

    napravio gluposti

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 const db = new pg.Client({
     user: "postgres",
@@ -17,10 +18,38 @@ const db = new pg.Client({
 db.connect();
 
 const API_address = "https://openlibrary.org/search.json?q="
+const API_address_covers = "https://covers.openlibrary.org/b/isbn/"
+
+// covers ne radi
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
 
 app.post('/new', async (req, res) => {
-    const input = req.body.query;
-    const result = await fetch(API_address + input);
+    const input = req.body;
+    const result = await fetch(API_address + input.title);
+    const data = await result.json();
+    const docs = data.docs;
+    let isbn = null;
+    data.docs.forEach(doc => {
+      if (doc.ia) {
+        doc.ia.forEach(item => {
+          if (item.startsWith("isbn_")) {
+            isbn = item.replace('isbn_', '');
+          }
+        });
+      }
+    });
+    const cover = API_address_covers + isbn + '.jpg';
+
+    res.render('index', {
+      title: input.title,
+      author: data.docs[0].author_name[0],
+      notes: input.notes,
+      date: input.date,
+      cover: cover
+    });
 });
 
 app.listen(port, () => {
